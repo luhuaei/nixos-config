ae30a91a0b8faba7aab19e414754aae53f818613
luhuaei <luhuaei@gmail.com>
2021-08-31 21:00:20 +0800

    update x

--- x.nix
+++ x.nix
@@ -5,22 +5,31 @@
   services.xserver = {
     enable = true;
     desktopManager = {
-      xfce = {
+      plasma5 = {
         enable = true;
+      };
+    };
+    displayManager.lightdm = {
+      greeters = {
+        mini = {
+          enable = true;
+          user = "redeveder";
+        };
       };
     };
     libinput.enable = true;
     layout = "us";
     xkbOptions = "eurosign:e";
   };
-  services.picom = {
-    enable = true;
-    fade = true;
-    inactiveOpacity = 0.9;
-    shadow = true;
-    fadeDelta = 4;
-  };
+
 
   services.upower.enable = true;
   systemd.services.upower.enable = true;
+
+  services.gpm.enable = true;
+  services.kmscon = {
+    enable = true;
+    hwRender = true;
+    extraConfig = "font-size=18";
+  };
 }
