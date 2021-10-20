7119996a3848d4de76f02783322fee6d6d5b6c68
luhuaei <luhuaei@gmail.com>
2021-10-20 20:29:21 +0800

    config nur in user

--- user/config.nix
+++ user/config.nix
@@ -2,4 +2,9 @@
 
 {
   allowUnfree = true;
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
 }
